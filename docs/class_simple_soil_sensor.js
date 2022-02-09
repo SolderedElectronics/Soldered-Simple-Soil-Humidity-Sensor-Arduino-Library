5e37b7aeaeb5779a791db256bc8d8ba4b1b7bcdd
Docs Robot <robot@example.com>
2022-02-09 15:24:07 +0000

    Committing documentation changes

--- docs/class_simple_soil_sensor.js
+++ docs/class_simple_soil_sensor.js
@@ -6,6 +6,7 @@ var class_simple_soil_sensor =
     [ "getResistance", "class_simple_soil_sensor.html#a9121b3e67b460ded8b1995d93b98a3f8", null ],
     [ "getValue", "class_simple_soil_sensor.html#a3597365f0cc03a9cd1ae335592220615", null ],
     [ "initializeNative", "class_simple_soil_sensor.html#a64caacd7688fdb3a862c5db9fcf7d256", null ],
+    [ "ADC_width", "class_simple_soil_sensor.html#a78792640833ca503a46b7ed78c21f1e7", null ],
     [ "pin", "class_simple_soil_sensor.html#a9a2e0d4a020b2828f66a9c32c8494897", null ],
     [ "raw", "class_simple_soil_sensor.html#af8c9af8f334b92bda0377b0d86ab2520", null ]
 ];
--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['address_0',['address',['../class_s_i_m_p_l_e___s_o_i_l___s_e_n_s_o_r___e_a_s_y_c_1_1_easy_c.html#ac9a4f9cd6290c7515a99baa3a6c93790',1,'SIMPLE_SOIL_SENSOR_EASYC::EasyC']]]
+  ['adc_5fwidth_0',['ADC_width',['../class_simple_soil_sensor.html#a78792640833ca503a46b7ed78c21f1e7',1,'SimpleSoilSensor']]],
+  ['address_1',['address',['../class_s_i_m_p_l_e___s_o_i_l___s_e_n_s_o_r___e_a_s_y_c_1_1_easy_c.html#ac9a4f9cd6290c7515a99baa3a6c93790',1,'SIMPLE_SOIL_SENSOR_EASYC::EasyC']]]
 ];

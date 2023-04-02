8f83e64652f5e0de5db031f7f5510be2046e0eda
KarloLeksic <lekxka@gmail.com>
2023-04-02 12:48:24 +0200

    new readme, added comments in attiny firmware

--- extras/attiny_firmware/attiny_firmware.ino
+++ extras/attiny_firmware/attiny_firmware.ino
@@ -14,49 +14,64 @@
 #include "easyC.h"
 #include <Wire.h>
 
-int addr = DEFAULT_ADDRESS;
+int addr = DEFAULT_ADDRESS; // 0x30
 
-volatile uint16_t readValue;
-byte threshold;
+volatile uint16_t readValue; // Variable for reading an analog value
+byte threshold;              // Variable for setting the threshold
+
+// Define pins for the sensor and LED
+#define LED_PIN    PA4
+#define SENSOR_PIN PA5
 
 void setup()
 {
+    // Set address switches as inputs, and get the I2C address
     initDefault();
     addr = getI2CAddress();
 
+    // Init I2C communication
     Wire.begin(addr);
     Wire.onReceive(receiveEvent);
     Wire.onRequest(requestEvent);
-    pinMode(PA4, OUTPUT);
-    digitalWrite(PA4, HIGH);
 
-    Serial.begin(115200);
+    // Set the LED pin as output and turn it on
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, HIGH);
 }
 
 void loop()
 {
-    readValue = analogRead(PA5);
+    // Read analog value
+    readValue = analogRead(SENSOR_PIN);
+
+    // Turn on the LED if the read value is greater than the threshold value
     if (readValue > (threshold * 0.01 * 1024))
     {
-        digitalWrite(PA4, HIGH);
+        digitalWrite(LED_PIN, HIGH);
     }
     else
     {
-        digitalWrite(PA4, LOW);
+        digitalWrite(LED_PIN, LOW);
     }
+
+    // Delay between readings
     delay(20);
 }
 
+// This function will be executed when Dasduino sends data to the breakout
 void receiveEvent(int howMany)
 {
+    // The breakout only receives a threshold value so read it and store
     if (Wire.available())
     {
         threshold = Wire.read();
     }
 }
 
+// This function will be executed when Dasduino requests data from the breakout
 void requestEvent()
 {
-    byte *valueForSend = (byte *)&readValue;
-    Wire.write(valueForSend, 2);
+    // Here is needed to convert the read value into bytes appropriate for sending
+    byte *valueForSend = (byte *)&readValue; // One uint16_t converted to 2 uint8_t
+    Wire.write(valueForSend, 2);             // Send 2 uint8_t (bytes)
 }
